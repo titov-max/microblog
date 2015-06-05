dba4c1a45cf58bd503b37fb62d57bad4ee104d7e
Titov Max <mtitov92@gmail.com>
2015-06-05 21:53:08 +0300

    Login Form. OpenID authorization

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from flask import render_template
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,6 +20,17 @@ def index():
         }
     ]
 	return render_template("index.html",
-		title = 'Home',
+		title = u'Главная',
 		user = user,
 		posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+		return redirect('/index')
+	return render_template("login.html",
+		title = u'Авторизация',
+		form = form,
+		providers = app.config['OPENID_PROVIDERS'])
